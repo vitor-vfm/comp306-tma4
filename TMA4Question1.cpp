f5cfb62ee73bcd6691ec933b6ff9590abc0c62c0
vitor-vfm <vitor.vfm@gmail.com>
2017-08-10 20:51:42 -0600

    Change Point to struct and add shapes
    Need to implement Square and Triangle

--- TMA4Question1.cpp
+++ TMA4Question1.cpp
@@ -1,40 +1,154 @@
 #include <iostream>
+#include <math.h>
+#include <vector>
+#include <array>
 
-class Point {
-private:
+struct Point {
     double x;
     double y;
 
-public:
     Point(double px, double py) : x(px), y(py) {}
 
-    friend std::ostream&
-    operator<<(std::ostream& st, const Point& point);
+    friend
+    std::ostream& operator<<(std::ostream& st, const Point& point);
 
-    Point
-    operator+(const Point& other) {
+    Point operator+(const Point& other) {
         return Point(x + other.x, y + other.y);
     }
 
-    Point
-    operator-(const Point& other) {
+    Point operator-(const Point& other) {
         return Point(x - other.x, y - other.y);
+    }
+
+    bool operator==(const Point& other) const {
+        return x == other.x && y == other.y;
+    }
+
+    bool operator!=(const Point& other) const {
+        return !(*this == other);
     }
 };
 
 std::ostream&
 operator<<(std::ostream& st, const Point& point) {
-    return st << "Point(" << point.x << ',' << point.y << ')';
+    return st << '(' << point.x << ',' << point.y << ')';
 }
 
+struct Shape {
+
+    virtual
+    double area() = 0;
+
+    virtual
+    double circumference() = 0;
+
+    virtual
+    std::pair<Point, Point> boundingBox() = 0;
+
+    void display() {
+        auto box = boundingBox();
+        std::cout << "(area: " << area() << ", ";
+        std::cout << "circumference: " << circumference() << ", ";
+        std::cout << "top left: " << box.first << ", ";
+        std::cout << "bottom right: " << box.second << ")\n";
+    }
+};
+
+class Circle : public Shape {
+private:
+    Point _centre;
+    double _radius;
+
+public:
+    Circle(Point centre, double radius) : _centre(centre), _radius(radius) {}
+
+    double area() {
+        return M_PI * _radius * _radius;
+    }
+
+    double circumference() {
+        return 2 * M_PI * _radius;
+    }
+
+    std::pair<Point, Point> boundingBox() {
+        Point topLeft(_centre.x - _radius, _centre.y + _radius);
+        Point bottomRight(_centre.x + _radius, _centre.y - _radius);
+        return std::make_pair(topLeft, bottomRight);
+    }
+
+    void display() {
+        std::cout << "Circle";
+        Shape::display();
+    }
+
+};
+
+class Square : public Shape {
+private:
+    std::array<Point, 4> points;
+
+public:
+    Square(Point p1, Point p2, Point p3, Point p4) :
+    /* The points must be in clockwise order */
+    points({p1,p2,p3,p4}) {}
+
+    double area() {
+        // TODO: implement
+        return 0;
+    }
+
+    double circumference() {
+        // TODO: implement
+        return 0;
+    }
+
+    std::pair<Point, Point> boundingBox() {
+        // TODO: implement
+        return std::make_pair(Point(0,0), Point(0,0));
+    }
+
+    void display() {
+        std::cout << "Square";
+        Shape::display();
+    }
+};
+
+class Triangle : public Shape {
+private:
+    std::array<Point, 3> points;
+
+public:
+    Triangle(Point p1, Point p2, Point p3) :
+    /* The points must be in clockwise order */
+    points({p1,p2,p3}) {}
+
+    double area() {
+        // TODO: implement
+        return 0;
+    }
+
+    double circumference() {
+        // TODO: implement
+        return 0;
+    }
+
+    std::pair<Point, Point> boundingBox() {
+        // TODO: implement
+        return std::make_pair(Point(0,0), Point(0,0));
+    }
+
+    void display() {
+        std::cout << "Triangle";
+        Shape::display();
+    }
+};
 
 int main() {
-    Point p1(2,3);
-    Point p2(4,5);
+    Circle circle(Point(10,5), 23);
+    Square square(Point(-10,7), Point(-6,12), Point(4,23), Point(5,-5));
+    Triangle triangle(Point(-15,15), Point(10,10), Point(0,0));
 
-    std::cout << p1 << '\n';
-    std::cout << p1+p2 << '\n';
-    std::cout << p1-p2 << '\n';
-    std::cout << p1+p2-p1 << '\n';
-
+    circle.display();
+    square.display();
+    triangle.display();
 }
