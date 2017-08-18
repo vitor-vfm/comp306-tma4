38783d4ec71c96c171b4429dfa0c9b74e825c7ca
vitor-vfm <vitor.vfm@gmail.com>
2017-08-18 15:50:24 -0600

    Question 1 docs

--- TMA4Question1.cpp
+++ TMA4Question1.cpp
@@ -1,3 +1,63 @@
+/*
+ Title: TMA4Question1.cpp
+ Description: Solution for Question 1 in TMA4
+ Date: August 18, 2017
+ Author: Vitor Mendonca
+*/
+
+/*
+ DOCUMENTATION
+
+ Program Purpose:
+    Create and use a hierarchy of classes that perform the functions
+    of geometric shapes, in order to study how inheritance works in C++
+
+ Compile (assuming Cygwin is running): make q1
+ Execution (assuming Cygwin is running): ./q1
+
+ Notes:
+    - In Cygwin, main must return type int
+    - Assumes C++11 (see Makefile for reference)
+
+ Classes:
+    Point: a class to represent a point in two-dimensional
+           space.
+    Shape: an abstract class to represent a shape. It
+           includes abstract member functions to calculate
+           the shape's area, its circumference, and its
+           bounding box (represented as a pair of Points)
+    Circle: a subclass of Shape that represents a circle.
+    Square: a subclass of Shape that represents a four-sided
+            polygon.
+    Triangle: a subclass of Shape that represents a three-sided
+            polygon.
+
+ Variables:
+    circle: a Circle instance for testing
+    square: a Square instance for testing
+    triangle: a Triangle instance for testing
+*/
+
+/*
+ TEST PLAN
+
+ Normal case:
+    >./q1
+    >Circle(area: 1661.9, circumference: 144.513, top left: (-13,28), bottom right: (33,-18))
+    >Square(area: 207, circumference: 68.4964, top left: (-10,23), bottom right: (5,-5))
+    >Triangle(area: 150, circumference: 60.8504, top left: (-15,15), bottom right: (10,0))
+
+ Discussion:
+    The main function in this file has the purpose of testing
+    the behaviour of the various classes. Test instances are
+    created with the start values mentioned in the TMA spec.
+    Their data is displayed using their corresponding member
+    functions.
+
+    This program takes no input.
+
+*/
+
 #include "point.h"
 #include "polygon.h"
 
